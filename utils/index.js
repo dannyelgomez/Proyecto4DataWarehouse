11fd4b1f3e5b66e1ad0426248866ba22b23b79c0
Daniel Gomez <dannyelgomez@hotmail.com>
2021-07-11 12:37:45 -0500

    index js refactor

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,10 @@
-const { model } = require('mongoose');
-const connMongoose = require('../../store/conexion');
 const modelCompanies = require('../components/companies_comp/company');
 
-
 function searchCompany(data) {
-    modelCompanies.find({ nit: data })
-        .then((result) => {
-            if (!result) {
-                console.log('La empresa existe');
-
-            } else {
-
-            }
-        })
+  modelCompanies.find({ nit: data })
+    .then((result) => {
+      if (!result) {
+        console.log('La empresa existe');
+      }
+    })
 }
